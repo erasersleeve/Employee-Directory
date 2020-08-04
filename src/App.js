e9a7cdcd693c8d109cc718650ec8f2393c0fa182
Tim Winters <61638296+erasersleeve@users.noreply.github.com>
2020-08-04 09:58:19 -0400

    Added initial navbar component, search functionality next

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
 import Directory from "./components/Directory/index";
+import Navbar from "./components/Navbar/index";
 import './App.css';
 
 function App() {
+  // const [list, setlist] = useState(directory);
+  
   return (
     <>
+    <Navbar />
     <Directory />
       </>
   );
